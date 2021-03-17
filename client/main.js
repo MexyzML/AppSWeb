cb81910f0edac144f20c70810ca60945e4570716
MexyzML <marin.legrand@epsi.fr>
2021-03-17 14:25:21 +0100

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,19 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.home.onCreated(function homeOnCreated() {
+    let ctrl = this;
+    this.movies = new ReactiveVar();
+    HTTP.call('GET', 'http://localhost:3000/api/discover/movie', {},
+        function(error, response) {
+            // Handle the error or response here.
+            ctrl.movies.set(JSON.parse(response.content).results)
+        });
+});
+Template.home.helpers({
+    movies() {
+        return Template.instance().movies.get();
+    }
+});
---
+++ server/main.js
@@ -0,0 +1,13 @@
+import { Meteor } from 'meteor/meteor';
+import { localDatas } from './local-datas.js';
+
+
+
+Meteor.startup(() => {
+    // If the Links collection is empty, add some data.
+});
+
+WebApp.connectHandlers.use('/api/discover/movie', (req, res, next) => {
+    res.writeHead(200);
+    res.end(JSON.stringify(localDatas));
+});
